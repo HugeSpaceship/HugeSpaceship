a93026c3c3b3af4da3018fa18689344c721d2e8d
Henry Asbridge <henry@vorax.org>
2023-06-21 19:36:57 +0100

    Stubbed publish handler

--- pkg/api/game_api/controllers/slots/upload.go
+++ pkg/api/game_api/controllers/slots/upload.go
@@ -1,5 +1,18 @@
 package slots
 
-func UploadLevel() {
+import (
+	"HugeSpaceship/pkg/common/model/lbp_xml"
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+)
 
+func StartPublishHandler() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		slot := lbp_xml.Slot{}
+		err := ctx.BindXML(&slot)
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to parse xml body")
+		}
+		ctx.XML(200, lbp_xml.Slot{Resource: []string{slot.RootLevel}, Type: "user"})
+	}
 }
