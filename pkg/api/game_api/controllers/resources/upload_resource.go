6f30ce202dbbcc88103fea68a6c00302131b2239
Henry Asbridge <henry@vorax.org>
2023-06-21 19:37:06 +0100

    Stubbed resource upload handler

--- pkg/api/game_api/controllers/resources/upload_resource.go
+++ pkg/api/game_api/controllers/resources/upload_resource.go
@@ -1,7 +1,18 @@
 package resources
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+	"io"
+	"os"
+)
 
 func UploadResources() gin.HandlerFunc {
-
+	return func(ctx *gin.Context) {
+		bytes, _ := io.ReadAll(ctx.Request.Body)
+		err := os.WriteFile(ctx.Param("hash"), bytes, 0644)
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to write file")
+		}
+	}
 }
