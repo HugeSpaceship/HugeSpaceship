10ebbe3ba4b95a40e570e9ec5a87e99cedaa49c8
Henry Asbridge <henry@vorax.org>
2023-06-21 20:23:17 +0100

    Fix remote config test

--- pkg/common/config/remote_config_test.go
+++ pkg/common/config/remote_config_test.go
@@ -3,15 +3,19 @@ package config
 import (
 	"HugeSpaceship/pkg/common/config/model/remote"
 	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
 	"testing"
 )
 
 func TestGetLBPAPIConfig(t *testing.T) {
-
+	viper.SetEnvPrefix("hs")
+	viper.AutomaticEnv()
 	GetLBPAPIConfig()
 }
 
 func TestSaveLBPAPIConfig(t *testing.T) {
+	viper.SetEnvPrefix("hs")
+	viper.AutomaticEnv()
 	config := remote.LBPAPIConfig{
 		PrimaryDigest:   "test1",
 		AlternateDigest: "test2",
