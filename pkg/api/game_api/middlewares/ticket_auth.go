5321776bb3821aac1ce5feb5251312426f096c43
Henry Asbridge <henry@vorax.org>
2023-07-08 15:20:07 +0100

    Fixed HTTP response on invalid ticket, should force the game to reauth

--- pkg/api/game_api/middlewares/ticket_auth.go
+++ pkg/api/game_api/middlewares/ticket_auth.go
@@ -3,20 +3,21 @@ package middlewares
 import (
 	"HugeSpaceship/pkg/common/db/auth"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func TicketAuthMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		token, err := ctx.Cookie("MM_AUTH") // TODO: double check that this is the right cookie?
 		if err != nil {
-			ctx.AbortWithStatus(401)
+			ctx.AbortWithStatus(http.StatusForbidden)
 			return
 		}
 
 		session, exists := auth.GetSession(token)
 
 		if !exists {
-			ctx.AbortWithStatus(401)
+			ctx.AbortWithStatus(http.StatusForbidden)
 			return
 		}
 
