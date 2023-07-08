99bf73765166016cf7adf75974fc60528e9d49cb
Henry Asbridge <henry@vorax.org>
2023-07-08 15:21:13 +0100

    WIP File logging

--- pkg/common/logger/logging.go
+++ pkg/common/logger/logging.go
@@ -10,9 +10,11 @@ import (
 func LoggingInit(service string) {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
-	log.Logger = log.With().Str("service", service).Caller().Logger()
-
 	if viper.GetBool("debug") {
-		log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+		log.Logger = log.With().Caller().Logger().Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	} else {
+		file, _ := os.OpenFile("./hs-"+service+".log", os.O_CREATE|os.O_WRONLY, 0644)
+		log.Logger = log.Level(zerolog.InfoLevel).With().Str("service", service).Logger().Output(file)
+
 	}
 }
