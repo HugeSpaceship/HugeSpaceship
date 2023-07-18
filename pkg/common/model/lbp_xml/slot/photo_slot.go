d8e5983067c39b93e20bd2caf909301b2fec7d8d
Henry Asbridge <henry@vorax.org>
2023-07-18 23:50:50 +0100

    Added slot type for photos

---
+++ pkg/common/model/lbp_xml/slot/photo_slot.go
@@ -0,0 +1,11 @@
+package slot
+
+import "encoding/xml"
+
+type PhotoSlot struct {
+	XMLName     xml.Name `xml:"slot"`
+	Type        string   `xml:"type,attr" db:"-"`
+	ID          int64    `xml:"id"`
+	Name        string   `xml:"name,omitempty"`
+	Description string   `xml:"description,omitempty"`
+}
