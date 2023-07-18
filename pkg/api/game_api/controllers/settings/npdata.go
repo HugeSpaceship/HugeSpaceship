76d9eaccbeeb66ed1d80fbba4ad48bced09b6dea
Henry Asbridge <henry@vorax.org>
2023-07-18 16:24:46 +0100

    Removed verbose logging for npdata

--- pkg/api/game_api/controllers/settings/npdata.go
+++ pkg/api/game_api/controllers/settings/npdata.go
@@ -2,17 +2,10 @@ package settings
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/rs/zerolog/log"
-	"io"
 )
 
 func NpDataEndpoint() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		data, err := io.ReadAll(ctx.Request.Body)
-		if err != nil {
-			panic("lul")
-		}
-		log.Info().Msg(string(data)) // TODO: Fuck you lbp, your protocol sucks
-		ctx.String(200, "sure buddy")
+		//TODO: Add NPData support for matching
 	}
 }
