87650371ad5c5ee7b680cc6b62348f74a537fd31
nicholas1250 <nglgtha1250@outlook.com>
2023-07-20 20:53:16 +0200

    fixed location in slot structs

--- pkg/common/model/lbp_xml/slot/search_slot.go
+++ pkg/common/model/lbp_xml/slot/search_slot.go
@@ -1,7 +1,7 @@
 package slot
 
 import (
-	"HugeSpaceship/pkg/common/model/lbp_xml"
+	"HugeSpaceship/pkg/common/model"
 	"HugeSpaceship/pkg/common/model/lbp_xml/npdata"
 	"encoding/xml"
 )
@@ -10,39 +10,39 @@ type SearchSlot struct {
 	XMLName xml.Name `xml:"slot"`
 	Type    string   `xml:"type,attr" db:"-"`
 
-	ID                  string           `xml:"id"`
-	NPHandle            npdata.NpHandle  `xml:"npHandle"`
-	SearchScore         float32          `xml:"searchScore"`
-	Location            lbp_xml.Location `xml:"location"`
-	Game                int              `xml:"game"`
-	Name                string           `xml:"name"`
-	Description         string           `xml:"description"`
-	RootLevel           string           `xml:"rootLevel"`
-	Icon                string           `xml:"icon"`
-	InitiallyLocked     bool             `xml:"initiallyLocked"`
-	IsSubLevel          bool             `xml:"isSubLevel"`
-	IsLBP1Only          bool             `xml:"isLBP1Only"`
-	Shareable           int              `xml:"shareable"`
-	MinPlayers          uint             `xml:"minPlayers"`
-	MaxPlayers          uint             `xml:"maxPlayers"`
-	HeartCount          int64            `xml:"heartCount"`
-	Thumbsup            int64            `xml:"thumbsup"`
-	Thumbsdown          int64            `xml:"thumbsdown"`
-	AverageRating       float32          `xml:"averageRating"`
-	PlayerCount         uint64           `xml:"playerCount"`
-	MatchingPlayers     uint64           `xml:"matchingPlayers"`
-	MMPick              bool             `xml:"mmpick"`
-	IsAdventurePlanet   bool             `xml:"isAdventurePlanet"`
-	Ps4Only             bool             `xml:"ps4Only"`
-	PlayCount           uint64           `xml:"playCount"`
-	CompletionCount     uint64           `xml:"completionCount"`
-	Lbp1PlayCount       uint64           `xml:"lbp1PlayCount"`
-	Lbp1CompletionCount uint64           `xml:"lbp1CompletionCount"`
-	Lbp1UniquePlayCount uint64           `xml:"lbp1UniquePlayCount"`
-	Lbp2PlayCount       uint64           `xml:"lbp2PlayCount"`
-	Lbp2CompletionCount uint64           `xml:"lbp2CompletionCount"`
-	UniquePlayCount     uint64           `xml:"uniquePlayCount"`
-	Lbp3PlayCount       uint64           `xml:"lbp3PlayCount"`
-	Lbp3CompletionCount uint64           `xml:"lbp3CompletionCount"`
-	Lbp3UniquePlayCount uint64           `xml:"lbp3UniquePlayCount"`
+	ID                  string          `xml:"id"`
+	NPHandle            npdata.NpHandle `xml:"npHandle"`
+	SearchScore         float32         `xml:"searchScore"`
+	Location            model.Location  `xml:"location"`
+	Game                int             `xml:"game"`
+	Name                string          `xml:"name"`
+	Description         string          `xml:"description"`
+	RootLevel           string          `xml:"rootLevel"`
+	Icon                string          `xml:"icon"`
+	InitiallyLocked     bool            `xml:"initiallyLocked"`
+	IsSubLevel          bool            `xml:"isSubLevel"`
+	IsLBP1Only          bool            `xml:"isLBP1Only"`
+	Shareable           int             `xml:"shareable"`
+	MinPlayers          uint            `xml:"minPlayers"`
+	MaxPlayers          uint            `xml:"maxPlayers"`
+	HeartCount          int64           `xml:"heartCount"`
+	Thumbsup            int64           `xml:"thumbsup"`
+	Thumbsdown          int64           `xml:"thumbsdown"`
+	AverageRating       float32         `xml:"averageRating"`
+	PlayerCount         uint64          `xml:"playerCount"`
+	MatchingPlayers     uint64          `xml:"matchingPlayers"`
+	MMPick              bool            `xml:"mmpick"`
+	IsAdventurePlanet   bool            `xml:"isAdventurePlanet"`
+	Ps4Only             bool            `xml:"ps4Only"`
+	PlayCount           uint64          `xml:"playCount"`
+	CompletionCount     uint64          `xml:"completionCount"`
+	Lbp1PlayCount       uint64          `xml:"lbp1PlayCount"`
+	Lbp1CompletionCount uint64          `xml:"lbp1CompletionCount"`
+	Lbp1UniquePlayCount uint64          `xml:"lbp1UniquePlayCount"`
+	Lbp2PlayCount       uint64          `xml:"lbp2PlayCount"`
+	Lbp2CompletionCount uint64          `xml:"lbp2CompletionCount"`
+	UniquePlayCount     uint64          `xml:"uniquePlayCount"`
+	Lbp3PlayCount       uint64          `xml:"lbp3PlayCount"`
+	Lbp3CompletionCount uint64          `xml:"lbp3CompletionCount"`
+	Lbp3UniquePlayCount uint64          `xml:"lbp3UniquePlayCount"`
 }
--- pkg/common/model/lbp_xml/slot/upload_slot.go
+++ pkg/common/model/lbp_xml/slot/upload_slot.go
@@ -1,28 +1,28 @@
 package slot
 
 import (
-	"HugeSpaceship/pkg/common/model/lbp_xml"
+	"HugeSpaceship/pkg/common/model"
 	"encoding/xml"
 )
 
 type Upload struct {
-	XMLName         xml.Name         `xml:"slot" `
-	Type            string           `xml:"type,attr" `
-	Name            string           `xml:"name,omitempty"`
-	Description     string           `xml:"description,omitempty"`
-	Icon            string           `xml:"icon,omitempty"`
-	RootLevel       string           `xml:"rootLevel,omitempty"`
-	Resources       []string         `xml:"resource"`
-	Location        lbp_xml.Location `xml:"location,omitempty"`
-	InitiallyLocked bool             `xml:"initiallyLocked,omitempty"`
-	IsSubLevel      bool             `xml:"isSubLevel,omitempty"`
-	IsLBP1Only      bool             `xml:"isLBP1Only,omitempty"`
-	Shareable       int              `xml:"shareable,omitempty"`
-	Background      string           `xml:"background,omitempty"`
-	Links           string           `xml:"links,omitempty"`
-	InternalLinks   string           `xml:"internalLinks,omitempty"`
-	LevelType       string           `xml:"levelType,omitempty"`
-	MinPlayers      int              `xml:"minPlayers,omitempty"`
-	MaxPlayers      int              `xml:"maxPlayers,omitempty"`
-	MoveRequired    bool             `xml:"moveRequired,omitempty"`
+	XMLName         xml.Name       `xml:"slot" `
+	Type            string         `xml:"type,attr" `
+	Name            string         `xml:"name,omitempty"`
+	Description     string         `xml:"description,omitempty"`
+	Icon            string         `xml:"icon,omitempty"`
+	RootLevel       string         `xml:"rootLevel,omitempty"`
+	Resources       []string       `xml:"resource"`
+	Location        model.Location `xml:"location,omitempty"`
+	InitiallyLocked bool           `xml:"initiallyLocked,omitempty"`
+	IsSubLevel      bool           `xml:"isSubLevel,omitempty"`
+	IsLBP1Only      bool           `xml:"isLBP1Only,omitempty"`
+	Shareable       int            `xml:"shareable,omitempty"`
+	Background      string         `xml:"background,omitempty"`
+	Links           string         `xml:"links,omitempty"`
+	InternalLinks   string         `xml:"internalLinks,omitempty"`
+	LevelType       string         `xml:"levelType,omitempty"`
+	MinPlayers      int            `xml:"minPlayers,omitempty"`
+	MaxPlayers      int            `xml:"maxPlayers,omitempty"`
+	MoveRequired    bool           `xml:"moveRequired,omitempty"`
 }
