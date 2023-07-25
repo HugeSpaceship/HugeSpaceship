b832502d12d3e6930216220f9d05a7f39de9f60f
Henry Asbridge <henry@vorax.org>
2023-07-26 00:55:44 +0100

    Added more fields to user struct

--- pkg/common/model/lbp_xml/user.go
+++ pkg/common/model/lbp_xml/user.go
@@ -4,13 +4,19 @@ import (
 	"HugeSpaceship/pkg/common/model/common"
 	"HugeSpaceship/pkg/common/model/lbp_xml/npdata"
 	"encoding/xml"
+	"github.com/google/uuid"
 )
 
 type User struct {
 	XMLName                   xml.Name        `xml:"user"`
+	ID                        uuid.UUID       `xml:"-" db:"id"`
+	PSN_UID                   string          `xml:"-" db:"psn_uid"`
+	RPCN_UID                  string          `xml:"-" db:"rpcn_uid"`
 	Type                      string          `xml:"type,attr"`
 	NpHandle                  npdata.NpHandle `xml:"npHandle"`
-	Biography                 string          `xml:"biography"`
+	Username                  string          `xml:"-" db:"username"`
+	AvatarHash                string          `xml:"-" db:"avatar_hash"`
+	Biography                 string          `xml:"biography" db:"bio"`
 	Game                      string          `xml:"game"`
 	Lbp1UsedSlots             string          `xml:"lbp1UsedSlots"`
 	EntitledSlots             string          `xml:"entitledSlots"`
@@ -31,8 +37,10 @@ type User struct {
 	CommentCount              string          `xml:"commentCount"`
 	PhotosByMeCount           string          `xml:"photosByMeCount"`
 	PhotosWithMeCount         string          `xml:"photosWithMeCount"`
-	CommentsEnabled           bool            `xml:"commentsEnabled"`
+	CommentsEnabled           bool            `xml:"commentsEnabled" db:"comments_enabled"`
 	Location                  common.Location `xml:"location"`
+	LocationX                 int32           `xml:"-" db:"locationx"`
+	LocationY                 int32           `xml:"-" db:"locationy"`
 	FavouriteSlotCount        string          `xml:"favouriteslotcount"`
 	FavouriteUserCount        string          `xml:"favouriteusercount"`
 	QueuedLevelCount          string          `xml:"lolcatftwcount"`
