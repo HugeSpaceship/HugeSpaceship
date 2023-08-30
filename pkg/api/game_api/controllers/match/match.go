39978ff6af9885da721a69f7ca85537eb16957ad
Henry Asbridge <henry@vorax.org>
2023-08-30 03:33:36 +0100

    Removed stub from match endpoint

--- pkg/api/game_api/controllers/match/match.go
+++ pkg/api/game_api/controllers/match/match.go
@@ -2,17 +2,10 @@ package match
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/rs/zerolog/log"
-	"io"
 )
 
 func MatchEndpoint() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		data, err := io.ReadAll(ctx.Request.Body)
-		if err != nil {
-			panic("lul")
-		}
-		log.Info().Msg(string(data)) // TODO: Fuck you lbp, your protocol sucks
-		ctx.String(200, "sure buddy")
+
 	}
 }
