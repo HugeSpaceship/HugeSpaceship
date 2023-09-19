8c0147a1bedbf05d5ef0ba08c9a21463f6bc9b60
Henry Asbridge <henry@vorax.org>
2023-09-19 01:11:36 +0100

    Add timer to logger to notify of slow requests

--- pkg/common/logger/gin_logger.go
+++ pkg/common/logger/gin_logger.go
@@ -1,23 +1,47 @@
 package logger
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
 	"time"
 )
 
+// LoggingMiddleware is a Gin middleware that logs to zerolog.
 func LoggingMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		start := time.Now()
-
-		// Process request
-		c.Next()
 
 		path := c.Request.URL.Path
 		query := c.Request.URL.RawQuery
 		if query != "" {
 			path = path + "?" + query
 		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+		defer cancel()
+
+		reqChan := make(chan struct{}, 1)
+
+		// So we know if there's a stuck request
+		//log.Debug().
+		//	Str("path", path).
+		//	Str("method", c.Request.Method).
+		//	Msg("Starting request")
+
+		// Check to see if the request was slow.
+		go func() {
+			select {
+			case <-ctx.Done():
+				log.Warn().Str("path", path).Msg("Slow Request, has taken 500ms so far")
+				return
+			case <-reqChan:
+			}
+		}()
+
+		// Process request
+		c.Next()
+		reqChan <- struct{}{}
 
 		log.Info().
 			Str("method", c.Request.Method).
